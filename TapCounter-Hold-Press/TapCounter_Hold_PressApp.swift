95c8bca989e9b23689c90eeadd5b9e51338f9bc1
DomLavonn <110582412+Domanikl@users.noreply.github.com>
2023-01-01 18:25:14 -0500

    Initial Commit

---
+++ TapCounter-Hold-Press/TapCounter_Hold_PressApp.swift
@@ -0,0 +1,17 @@
+//
+//  TapCounter_Hold_PressApp.swift
+//  TapCounter-Hold-Press
+//
+//  Created by Domanik Johnson on 1/1/23.
+//
+
+import SwiftUI
+
+@main
+struct TapCounter_Hold_PressApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
